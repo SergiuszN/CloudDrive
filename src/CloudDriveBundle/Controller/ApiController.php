cebc835b517813cc799c07d384cdcdfd6109e385
SergiuszN <serg.ni2011@gmail.com>
2016-10-10 22:07:28 +0200

    Main window controller with navigation file size and date last change

--- src/CloudDriveBundle/Controller/ApiController.php
+++ src/CloudDriveBundle/Controller/ApiController.php
@@ -8,40 +8,87 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ApiController extends Controller
 {
-    public function mainPageAction($path = '')
+    public function mainPageAction($path = 'home:')
     {
         /* @var User $user */
         $user = $this->getUser();
         $response = new JsonResponse();
 
-        // if user login
-        if (! $user) {
-            return $response;
-        }
+        // replace path variable
+        $path = str_replace('home:', '', $path);
+        $path = str_replace(':', '/', $path);
+        $path = str_replace('!', '..', $path);
 
-        $parameters = array(
-            'userId' => $user->getId(),
-            'path' => $path
-        );
+        // get user directory
+        $userDir = realpath(__DIR__ . '/../../../uploads/user' . $user->getId()) . '/';
+        $userDir = str_replace('\\', '/', $userDir);
 
-        $basePath = __DIR__ . '/../../../uploads/';
-        $userDir = $basePath . 'user' . $user->getId() . '/';
-        $pathToOpen = $userDir;
-
+        // get path to open
         if ($path) {
-            $path = str_replace('..', '', $path);
-            $path = str_replace(':', '/', $path);
-            $pathToOpen .= $path;
+            $pathToOpen = realpath($userDir . $path) . '/';
+            $pathToOpen = str_replace('\\', '/', $pathToOpen);
+        } else {
+            $pathToOpen = $userDir;
         }
 
+
+        // create user folder if not exist
         if (!file_exists($userDir)) {
             mkdir($userDir);
         }
 
-        $parameters['folder'] = $pathToOpen;
-        $parameters['dir'] = scandir($pathToOpen);
+        // safety block limited access over user folder
+        if (strrpos($pathToOpen, $userDir) === false) {
+            return $response;
+        }
+
+        // scan dir and remove '.' from list and '..' if is home folder
+        $list = scandir($pathToOpen);
+        array_shift($list);
+        if ($pathToOpen == $userDir) {
+            array_shift($list);
+        }
+
+        $dir = array();
+
+        foreach ($list as $key => $file) {
+            if (is_dir($pathToOpen . $file)) {
+                $dir['directory'][] = array($file, $this->folderSize($pathToOpen . $file), filemtime($pathToOpen . $file)*1000);
+            } else {
+                $dir['file'][] = array($file, filesize($pathToOpen . $file), filemtime($pathToOpen . $file)*1000);
+            }
+        }
+
+        $parameters = array(
+            'url' => $this->generateUrl('cloud_drive_api_main_page', array(), true) . '/',
+            'folder' => str_replace('/', ':', 'home:' . str_replace($userDir, '', $pathToOpen)),
+            'dir' => $dir,
+        );
+
+//        echo '<pre>';
+//        var_dump($parameters);
+//        echo '</pre>';
+        //die();
         $response->setData($parameters);
         return $response;
+    }
+
+    protected function folderSize($dir){
+        $count_size = 0;
+        $count = 0;
+        $dir_array = scandir($dir);
+        foreach($dir_array as $key=>$filename){
+            if($filename!=".." && $filename!="."){
+                if(is_dir($dir."/".$filename)){
+                    $new_folderSize = $this->folderSize($dir."/".$filename);
+                    $count_size = $count_size + $new_folderSize;
+                }else if(is_file($dir."/".$filename)){
+                    $count_size = $count_size + filesize($dir."/".$filename);
+                    $count++;
+                }
+            }
+        }
+        return $count_size;
     }
 
     /* @return UserRepository */
